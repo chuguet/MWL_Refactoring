f338311218fcf1506baa22e534af29070f68e5ef
Huguet <huguet10@gmail.com>
2015-04-06 22:59:43 +0200

    Se añade la clase LeftOpenedInterval que hereda de Interval

---
+++ src/intervals/LeftOpenedInterval.java
@@ -0,0 +1,9 @@
+package intervals;
+
+public class LeftOpenedInterval extends Interval {
+
+    public LeftOpenedInterval(double minimum, double maximum, Opening opening) {
+        super(minimum, maximum, opening);
+    }
+
+}
