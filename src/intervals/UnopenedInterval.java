c6310cae21bdddfc1ea1ee3aed0e3726ee8afea8
Huguet <huguet10@gmail.com>
2015-04-06 23:51:09 +0200

    Se implementa el metodo includes un intervalo en UnopenedInterval

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -24,4 +24,27 @@ public class UnopenedInterval extends Interval {
         return this.includes(interval.getMinimum()) || this.includes(interval.getMaximum());
     }
 
+    @Override
+    public boolean includes(Interval interval) {
+        boolean minimumIncluded = this.includes(interval.getMinimum());
+        boolean maximumIncluded = this.includes(interval.getMaximum());
+        switch (interval.getOpening()) {
+        case BOTH_OPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        case LEFT_OPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        case RIGHT_OPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        case UNOPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        default:
+            assert false;
+            return false;
+        }
+    }
+
 }
