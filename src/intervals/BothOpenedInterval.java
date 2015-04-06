53add9ff38d540f49913e70ea7d5e045a9a5af2f
Huguet <huguet10@gmail.com>
2015-04-06 23:42:06 +0200

    Se implementa el metodo includes un intervalo en BothOpenedInterval

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -22,4 +22,26 @@ public class BothOpenedInterval extends Interval {
         return this.includes(interval.getMinimum()) || this.includes(interval.getMaximum());
     }
 
+    @Override
+    public boolean includes(Interval interval) {
+        boolean minimumIncluded = this.includes(interval.getMinimum());
+        boolean maximumIncluded = this.includes(interval.getMaximum());
+        switch (interval.getOpening()) {
+        case BOTH_OPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        case LEFT_OPENED:
+            return (minimumIncluded || this.getMinimum() == interval.getMinimum())
+                    && (maximumIncluded);
+        case RIGHT_OPENED:
+            return (minimumIncluded)
+                    && (maximumIncluded || this.getMaximum() == interval.getMaximum());
+        case UNOPENED:
+            return (minimumIncluded) && (maximumIncluded);
+        default:
+            assert false;
+            return false;
+        }
+    }
+
 }
