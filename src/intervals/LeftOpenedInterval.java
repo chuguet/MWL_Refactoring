e4c49309edeffeb8d80084640477cbe914d2d529
Huguet <huguet10@gmail.com>
2015-04-06 23:27:02 +0200

    Se implementa el metodo intersectsWith en LeftOpenedInterval

--- src/intervals/LeftOpenedInterval.java
+++ src/intervals/LeftOpenedInterval.java
@@ -11,4 +11,18 @@ public class LeftOpenedInterval extends Interval {
         return this.getMinimum() < value && value <= this.getMaximum();
     }
 
+    @Override
+    public boolean intersectsWith(Interval interval) {
+
+        if (this.getMinimum() == interval.getMaximum()) {
+            return false;
+        }
+        if (this.getMaximum() == interval.getMinimum()) {
+            return interval.getOpening() == Opening.RIGHT_OPENED
+                    || interval.getOpening() == Opening.UNOPENED;
+        }
+        return this.includes(interval.getMinimum()) || this.includes(interval.getMaximum());
+
+    }
+
 }
