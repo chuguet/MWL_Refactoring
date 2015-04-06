4038e2aec2d055f3ac3293a4afc4e74ae34b2b3f
Huguet <huguet10@gmail.com>
2015-04-06 23:00:52 +0200

    Se añade la clase UnopenedInterval que hereda de Interval

---
+++ src/intervals/UnopenedInterval.java
@@ -0,0 +1,9 @@
+package intervals;
+
+public class UnopenedInterval extends Interval {
+
+    public UnopenedInterval(double minimum, double maximum, Opening opening) {
+        super(minimum, maximum, opening);
+    }
+
+}
