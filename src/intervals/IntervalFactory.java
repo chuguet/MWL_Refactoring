39ebcab3e787a4f1d42503ab360e4f31fb088b13
Huguet <huguet10@gmail.com>
2015-04-06 23:16:05 +0200

    Se modifica la factoria para que devuelva el tipo correcto de objeto en función del tipo de intervalo

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -1,9 +1,19 @@
 package intervals;
 
-
 public class IntervalFactory {
 
-	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
-	}
+    public static Interval getInterval(double minimum, double maximum, Opening opening) {
+        switch (opening) {
+        case BOTH_OPENED:
+            return new BothOpenedInterval(minimum, maximum, opening);
+        case LEFT_OPENED:
+            return new LeftOpenedInterval(minimum, maximum, opening);
+        case RIGHT_OPENED:
+            return new RightOpenedInterval(minimum, maximum, opening);
+        case UNOPENED:
+            return new UnopenedInterval(minimum, maximum, opening);
+        default:
+            throw new IllegalArgumentException("Unexpected type interval");
+        }
+    }
 }
