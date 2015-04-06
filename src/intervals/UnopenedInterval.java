ad462513a74cb0b89c5d11d28ad56e2cf871b20d
Huguet <huguet10@gmail.com>
2015-04-06 23:30:43 +0200

    Se implementa el metodo intersectsWith en UnopenedInterval

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -11,4 +11,18 @@ public class UnopenedInterval extends Interval {
         return this.getMinimum() <= value && value <= this.getMaximum();
     }
 
+    @Override
+    public boolean intersectsWith(Interval interval) {
+        if (this.getMinimum() == interval.getMaximum()) {
+            return interval.getOpening() == Opening.LEFT_OPENED
+                    || interval.getOpening() == Opening.UNOPENED;
+        }
+        if (this.getMaximum() == interval.getMinimum()) {
+            return interval.getOpening() == Opening.RIGHT_OPENED
+                    || interval.getOpening() == Opening.UNOPENED;
+        }
+        return this.includes(interval.getMinimum()) || this.includes(interval.getMaximum());
+
+    }
+
 }
