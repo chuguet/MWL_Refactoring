961a089fbcdd6be0df4b033309c201c8c9df304d
Huguet <huguet10@gmail.com>
2015-04-06 23:25:09 +0200

    Se implementa el metodo intersectsWith en BothOpenedInterval

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -11,4 +11,16 @@ public class BothOpenedInterval extends Interval {
         return this.getMinimum() < value && value < this.getMaximum();
     }
 
+    @Override
+    public boolean intersectsWith(Interval interval) {
+        if (this.getMinimum() == interval.getMaximum()) {
+            return false;
+        }
+        if (this.getMaximum() == interval.getMinimum()) {
+            return false;
+        }
+        return this.includes(interval.getMinimum()) || this.includes(interval.getMaximum());
+
+    }
+
 }
