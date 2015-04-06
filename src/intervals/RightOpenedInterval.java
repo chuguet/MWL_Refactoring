0c92f38558471992743f155d41486d7bc1c42c9e
Huguet <huguet10@gmail.com>
2015-04-06 22:58:25 +0200

    Se añade la clase RightOpenedInterval que hereda de Interval

---
+++ src/intervals/RightOpenedInterval.java
@@ -0,0 +1,9 @@
+package intervals;
+
+public class RightOpenedInterval extends Interval {
+
+    public RightOpenedInterval(double minimum, double maximum, Opening opening) {
+        super(minimum, maximum, opening);
+    }
+
+}
