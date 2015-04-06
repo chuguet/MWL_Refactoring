ffb6898322e10218f31678005675d01b6fc8b85e
Huguet <huguet10@gmail.com>
2015-04-07 00:05:20 +0200

    Se corrige el constructor de BothOpenedInterval pasando un Point

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -2,8 +2,8 @@ package intervals;
 
 public class BothOpenedInterval extends Interval {
 
-    public BothOpenedInterval(double minimum, double maximum, Opening opening) {
-        super(minimum, maximum, opening);
+    public BothOpenedInterval(Point point, Opening opening) {
+        super(point, opening);
     }
 
     @Override
