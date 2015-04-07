4f5edc351b84978a706662c95018e1c80513273e
Huguet <huguet10@gmail.com>
2015-04-07 23:04:41 +0200

    Se hacen todos los metodos abstractos de includes con la clase abstracta y las hijas para el doble despacho

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -19,6 +19,14 @@ public abstract class Interval {
 
     public abstract boolean includes(Interval interval);
 
+    public abstract boolean includes(BothOpenedInterval interval);
+
+    public abstract boolean includes(UnopenedInterval interval);
+
+    public abstract boolean includes(RightOpenedInterval interval);
+
+    public abstract boolean includes(LeftOpenedInterval interval);
+
     public abstract boolean intersectsWith(Interval interval);
 
     @Override
